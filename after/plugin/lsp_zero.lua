5c0af3b87b0542655fe374136b370f327ddcfc05
Rafael Marques <rafamdmarques@proton.me>
2024-02-18 16:49:13 -0300

    Add friendly-snippets

--- after/plugin/lsp_zero.lua
+++ after/plugin/lsp_zero.lua
@@ -1,64 +1,70 @@
 local lsp_zero = require('lsp-zero')
 local cmp = require('cmp')
 local cmp_action = lsp_zero.cmp_action()
+local cmp_format = require('lsp-zero').cmp_format()
+
+require("luasnip.loaders.from_vscode").lazy_load()
 
 lsp_zero.on_attach(function(client, bufnr)
-	-- see :help lsp-zero-keybindings
-	-- to learn the available actions
-	local opts = { buffer = bufnr }
+    -- see :help lsp-zero-keybindings
+    -- to learn the available actions
+    local opts = { buffer = bufnr }
 
-	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-	vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-	vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-	vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-	vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-	vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-	vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-	vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-	vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-	vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 
-	lsp_zero.default_keymaps({ buffer = bufnr })
+    lsp_zero.default_keymaps({ buffer = bufnr })
 end)
 
 lsp_zero.set_sign_icons({
-	error = '✘',
-	warn = '▲',
-	hint = '⚑',
-	info = '»'
+    error = '✘',
+    warn = '▲',
+    hint = '⚑',
+    info = '»'
 })
 
 lsp_zero.format_on_save({
-  format_opts = {
-    async = false,
-    timeout_ms = 10000,
-  },
-  servers = {
-    ['rust_analyzer'] = {'rust'},
-    ['tsserver'] = {'javascript', 'typescript'},
-  }
+    format_opts = {
+        async = false,
+        timeout_ms = 10000,
+    },
+    servers = {
+        ['rust_analyzer'] = { 'rust' },
+        ['tsserver'] = { 'javascript', 'typescript' },
+    }
 })
 
 cmp.setup({
-	sources = {
-		{ name = 'path' },
-		{ name = 'nvim_lsp' },
-		{ name = 'luasnip', keyword_length = 2 },
-		{ name = 'buffer',  keyword_length = 3 },
-	},
-	mapping = cmp.mapping.preset.insert({
-		['<CR>'] = cmp.mapping.confirm({ select = true }),
-		['<Tab>'] = cmp_action.luasnip_supertab(),
-		['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
-	}),
+    sources = {
+        { name = 'path' },
+        { name = 'buffer' },
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' },
+    },
+    mapping = cmp.mapping.preset.insert({
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ['<Tab>'] = cmp_action.luasnip_supertab(),
+        ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
+        ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+        ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+    }),
+    formatting = cmp_format,
 })
 
 -- to learn how to use mason.nvim with lsp-zero
 -- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guide/integrate-with-mason-nvim.md
 require('mason').setup({})
 require('mason-lspconfig').setup({
-	ensure_installed = {},
-	handlers = {
-		lsp_zero.default_setup,
-	},
+    ensure_installed = {},
+    handlers = {
+        lsp_zero.default_setup,
+    },
 })
--- lua/plugins/list.lua
+++ lua/plugins/list.lua
@@ -1,27 +1,29 @@
 return {
-	{
-		'nvim-treesitter/nvim-treesitter', build = 'TSUpdate' 
-	},
+    {
+        'nvim-treesitter/nvim-treesitter', build = 'TSUpdate'
+    },
 
-	{
-		'nvim-telescope/telescope.nvim', tag = '0.1.5',
-		dependencies = { 'nvim-lua/plenary.nvim' }
-	},
+    {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.5',
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    },
 
-	{'williamboman/mason.nvim'},
-	{'williamboman/mason-lspconfig.nvim'},
+    { 'williamboman/mason.nvim' },
+    { 'williamboman/mason-lspconfig.nvim' },
 
-	{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
-	{'neovim/nvim-lspconfig'},
-	{'hrsh7th/cmp-nvim-lsp'},
-	{'hrsh7th/nvim-cmp'},
-	{'L3MON4D3/LuaSnip'},
+    { 'VonHeikemen/lsp-zero.nvim',        branch = 'v3.x' },
+    { 'neovim/nvim-lspconfig' },
+    { 'hrsh7th/cmp-nvim-lsp' },
+    { 'hrsh7th/nvim-cmp' },
+    { 'L3MON4D3/LuaSnip',                 dependencies = { 'rafamadriz/friendly-snippets', 'honza/vim-snippets' } },
+    { 'saadparwaiz1/cmp_luasnip' },
 
-	{'nvim-treesitter/playground'},
-	{'mbbill/undotree'},
-	{'tpope/vim-fugitive'},
-	{'theprimeagen/harpoon'},
+    { 'nvim-treesitter/playground' },
+    { 'mbbill/undotree' },
+    { 'tpope/vim-fugitive' },
+    { 'theprimeagen/harpoon' },
 
-    {'folke/tokyonight.nvim'},
-    {'Shatur/neovim-ayu'},
+    { 'folke/tokyonight.nvim' },
+    { 'Shatur/neovim-ayu' },
 }
